54d41a4dc3a6e8ea7a93ee42e2456412a7416841
Anita-ihuman <charlesanita403@gmail.com>
2021-01-25 23:31:29 +0100

    working on the main component

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,20 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
+import Nav from "../pages/main/nav";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <nav className={styles.nav}>
-        <h1 className={styles.title}>
-          <a href="/">Gravity</a>
-        </h1>
-        <div className={styles.menu}>
-          <a href="/">Features</a>
-          <a href="/"> Pricing</a>
-          <a href="/">Why Gravity</a>
-          <a href="/">Sign In</a>
-        </div>
-      </nav>
-
+      <Nav />
       <main className={styles.main}>
-        <img src="/img.png" alt="Logo" className={styles.img} />
-        <div>
-          <h2>Simply time tracking Powerful reporting</h2>
+        <div className={styles.title}>
+          <h2>Simply time tracking. Powerful reporting.</h2>
           <p>
             Turn your team on to productivituy with Gravity the time tracker
           </p>
-          <button>Request early access</button>
+          <button className={styles.btn}>Request early access</button>
         </div>
+        <img src="/img.png" alt="Logo" className={styles.img} />
       </main>
 
       <footer className={styles.footer}>
