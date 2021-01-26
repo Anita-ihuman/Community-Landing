3507063cb8db6865af169ee401cd01651728a211
Anita-ihuman <charlesanita403@gmail.com>
2021-01-26 13:23:06 +0100

    adjust navbar

--- pages/main/nav.tsx
+++ pages/main/nav.tsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
 import styles from "./nav.module.scss";
 
- const Home: React.FC = () => {
+
+
+interface NavProps {
+  text: string;
+  option: boolean;
+  units: number;
+}
+
+const menu: React.FC<NavProps> = () => {
+  const [count, setCount] = useState(false);
+  const [selector, setSelector] = useState(false);
+  const toggle = () => setCount(!count);
+
+  const handleClick = () => setCount(!count);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setSelector(false);
+    } else {
+      setSelector(true);
+    }
+  };
   return (
-    
-    <nav className={styles.nav}>
-      <h1 className={styles.header}>
-        <a href="/">Gravity</a>
-      </h1>
-      <div className={styles.menu}>
-        <a href="/">Features</a>
-        <a href="/"> Pricing</a>
-        <a href="/">Why Gravity</a>
-        <a href="/">Sign In</a>
-      </div>
-    </nav>
-  )
- }
-export default Home;
+    <>
+      <nav className={styles.nav}>
+        <h1 className={styles.header}>
+          <a href="/">Gravity</a>
+        </h1>
+        <div onClick={handleClick}>
+          <p className={styles.toggle}>menu </p>
+        </div>
+
+        <div
+          className={count ? "nav-menu active" : "nav-menu"}
+          onMouseEnter={onMouseEnter}
+        >
+          <a href="/">Features</a>
+          <a href="/"> Pricing</a>
+          <a href="/">Why Gravity</a>
+          <a href="/">Sign In</a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default menu;
