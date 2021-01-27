b4dc334bfd0d4e5127ef6c74c5142897c389e496
Anita-ihuman <charlesanita403@gmail.com>
2021-01-27 01:14:12 +0100

    Fixed a bug  in nav bar

--- pages/main/Menu.tsx
+++ pages/main/Menu.tsx
@@ -3,24 +3,39 @@ import styles from "./nav.module.scss";
 
 const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const onClick = () => setNavOpen(true);
+
+  // const Results = () => (
+  //   <div id="results" className="search-results">
+
+  //   </div>
+  // );
+
   const handleNavToggle = () => {
     setNavOpen(!navOpen);
   };
   const renderClasses = () => {
     let classes = "menu";
     if (navOpen) {
-      classes += "active";
+      classes += { Results };
     }
+
     return classes;
   };
+
   return (
     <>
       <nav className={styles.nav}>
         <h1 className={styles.header}>
-          <a href="/">Gravity</a>
+          <img src="/Logo.png" alt="Logo" />
         </h1>
 
-        <ul className={styles.menu}>
+        <ul
+          className={styles.menu}
+          style={{
+            transform: navOpen ? "translateX(0px)" : "translateX(-700px)",
+          }}
+        >
           <li className={styles.link}>
             <a href="/">Features</a>
           </li>
@@ -31,13 +46,15 @@ const Navbar = () => {
             <a href="/">Why Gravity</a>
           </li>
           <li className={styles.link}>
-            <a href="/">Sign In</a>
+            <span className={styles.special}>
+              <a href="/">Sign In</a>
+            </span>
           </li>
         </ul>
         <div className={styles.toggle} onClick={handleNavToggle}>
-          <button className={styles.bars}>
+          <button className={styles.bars} onClick={() => setNavOpen}>
             <img src="/dots.png" alt="Logo" />
-          <span>MENU</span>
+            <span>MENU</span>
           </button>
         </div>
       </nav>
